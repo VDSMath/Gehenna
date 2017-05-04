fc2c502233c56b4754a907513c20e85e619918a8
VDSMath <matheusvdsdf@gmail.com>
2017-05-04 17:29:49 -0300

    Movimento do Acedia

---
+++ Assets/Scripts/Acedia.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class Acedia : MonoBehaviour {
+
+    public float speed;
+    private float step;
+    private GameObject target;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();	
+	}
+
+    void Move()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+}
