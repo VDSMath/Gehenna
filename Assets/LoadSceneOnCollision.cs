6d17f83489672a8080fe788da62bff1e25a55613
VDSMath <matheusvdsdf@gmail.com>
2017-06-01 16:29:00 -0300

    Adicionado fade

--- Assets/LoadSceneOnCollision.cs
+++ Assets/LoadSceneOnCollision.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnCollision : MonoBehaviour {
 
     private GameObject target;
+    public GameObject fade;
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
         target = other.gameObject;
         if (target.name == "player")
         {
+            fade.GetComponent<FadeObjectInOut>().FadeIn(fade.GetComponent<FadeObjectInOut>().fadeTime);
+            GameObject canvas = GameObject.Find("Canvas");
+            canvas.SetActive(false);
+            yield return new WaitForSeconds(fade.GetComponent<FadeObjectInOut>().fadeTime);
             SceneManager.LoadScene(2);
         }
     }
